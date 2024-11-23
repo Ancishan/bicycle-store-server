c520178068b7c44da97d0db45dd414b034774041
Ancishan <shantodev027@gmail.com>
2024-11-24 01:27:32 +0800

    [DONE] biCycle.model and interface

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,9 @@ import config from './config';
 
 async function server() {
     try {
-       await mongoose.connect('mongodb+srv://bicycle-store:HzxBNszLSGoWpc5c@cluster0.nhcslav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-        app.listen(5000, () => {
-            console.log("server running")
+       await mongoose.connect(config.database_url as string)
+        app.listen(config.port, () => {
+            console.log(`Server running on port ${config.port}`)
         })
 
     } catch (error) {
